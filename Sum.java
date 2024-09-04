02ccd1689ccc3d99957945733ffa66c3d2d3d06c
Mansu2022 <manjunathmouli@gmail.com>
2024-09-04 18:03:25 +0530

    commiting the modifications in Sum.java

--- Sum.java
+++ Sum.java
@@ -4,7 +4,8 @@ public class Sum {
     public static void main(String[] args) {
         int a = 5;
         int b = 6;
-        
+        //
         System.out.println("The sum is"+ (a+b));
+        
     }
 }
